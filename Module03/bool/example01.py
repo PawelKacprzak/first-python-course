381594789561144ffbda9cfcfe37a731f94ef228
Pawel Kacprzak <pkacprzak.edu@gmail.com>
2024-05-20 10:43:02 +0200

    add folder Module03 with folder bool and new example and exexercises files

---
+++ Module03/bool/example01.py
@@ -0,0 +1,21 @@
+val1 = True
+val2 = False
+
+print(val1)
+print(type(val1))
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
+print(not True)
+print(not False)
+print()
+print(bool('python'))
+print(bool(''))
+print(bool(0))
+print(bool(13))
---
+++ Module03/bool/exercise02.py
@@ -0,0 +1,6 @@
+x = 3
+y = 5
+z = 0
+
+print((x > y) and (z < y))
+print((x > y) or (z < y))
